5125883e5e08a7fb16d3cc3a97695cdcefc5c306
sahil-gusain <sahilgusainddn@gmail.com>
2023-02-13 20:01:24 +0530

    learning js

---
+++ javascript/index.js
@@ -0,0 +1,17 @@
+//Random no generater
+
+var n = MAth.random();
+/*
+A random no is genterated b/w 0-0.9999999999999999.
+It is pseudo random no generater 
+*/
+
+// console.log(n);
+
+/* program to generate random between 1-100*/
+
+var n = Math.random();
+n = n*100;
+n= Math.floor(n+1);
+
+console.log(n);
