2f64959b4fa37dd039481f6254e72cbc81a523b8
sahil-gusain <sahilgusainddn@gmail.com>
2023-02-14 21:26:33 +0530

    array's

--- javascript/index.js
+++ javascript/index.js
@@ -21,3 +21,24 @@ if(n===100){
     console.log("lovescore : " + n);
 }
 /* ==  checks for equality but does not cares about data type */
+
+/* ARRAY'S in javascript
+ An array is a single variable which can store multiple values of same type at a time
+
+ var arrayName = [value 1, value 2, …];
+ var arrayName = new Array();
+ var arrayName =new Array("element1","element2",..."elementn");
+
+ Array includes() method:
+ Use: To check whether the given array contains the specified element.*/
+
+var guestlist = ["Neha","Raj","Rahul","Sahil","Rakesh"];
+
+var name= prompt("What is your name");
+
+if(guestlist.includes(name))
+{
+    console.log("welcome");
+}else{
+    console.log("you are not invited");
+}
