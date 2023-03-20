072577048c66785ef0b4093324c8a6bb12497c6e
sahil-gusain <sahilgusainddn@gmail.com>
2023-03-21 00:18:41 +0530

    jquerybegin

---
+++ jQuery/index.js
@@ -0,0 +1,11 @@
+//jQuery is a fast, small, and feature-rich JavaScript library.
+//document.querySelector(".drum") is replaced by $(".drum") and jQuery(".drum")
+
+
+$("h1").css("color","red");
+
+//sometimes when we link jquery in the head it might take much longer to execute hence,
+//ready function is used or script link is provided in the beginning 
+//$("document").ready(function(){
+//     $("h1").css("color","red");
+// })
