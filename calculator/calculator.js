ca6bd6891744893d3bd3844b783990110c812029
sahil-gusain <sahilgusainddn@gmail.com>
2023-03-28 11:56:18 +0530

    calculators server

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+// allow us to pass the information.that we get sent from the post request.
+
+
 
 app.listen(8000,function(){
     console.log("sever running at port 80000");
 })
+app.post("/",function(req,res){
+    var num1 = Number(req.body.num1);//explicit conversion
+    var num2 = Number(req.body.num2);
+    res.send("Thankyou for submitting ans is "+(num1+num2));
+})
 
 app.get("/",function(req,res){
-    res.send("<h1>hello world</h1>");
+    res.sendFile(__dirname+"\\index.html");//send to used to send text wheareas sendFile is used 
+    //send File as a respond
+    //console.log(__dirname+"\\index.html");
 })
