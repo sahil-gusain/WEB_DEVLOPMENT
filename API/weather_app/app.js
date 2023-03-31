909d1245e00d7cb865004d88fc63d752635c273a
sahil-gusain <sahilgusainddn@gmail.com>
2023-03-31 14:10:09 +0530

    url fix

--- API/weather_app/app.js
+++ API/weather_app/app.js
@@ -5,7 +5,7 @@ const https = require("https");
 const app = express();
 
 app.get("/",function(req,res){
-    var url = "https://api.openweathermap.org/data/2.5/weather?q=delhi,India&APPID=7793601142131e844521fc537e7b9316";
+    url = " ";
     https.get(url,function(response){
         console.log(response);
     })
