604716297748bf9f3a8826a0c6cdd7859f14ad82
sahil-gusain <sahilgusainddn@gmail.com>
2023-04-05 21:18:54 +0530

    serverOP

--- newsletter/app.js
+++ newsletter/app.js
@@ -6,8 +6,16 @@ const request = require("request");
 
 const app=express();
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
 app.get("/",function(req,res){
-    res.send("server is running baby");
+    res.sendFile(__dirname+"//signup.html");
+})
+
+
+app.post("/",function(req,res){
+    res.send(req.body.first+" have been hacked");
 })
 
 app.listen(8000);
