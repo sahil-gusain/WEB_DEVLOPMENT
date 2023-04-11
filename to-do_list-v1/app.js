8aa22f8dbbf5f3ae1b2b1ff557b31eaa06a8e1ac
sahil-gusain <sahilgusainddn@gmail.com>
2023-04-11 20:46:40 +0530

    learning ejs

--- to-do_list-v1/app.js
+++ to-do_list-v1/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get("/",function (req,res) {
-    const today = new Date();
+    const today = new Date("2023-05-31");
     var day = today.getDay();
     var currday = "";
     // console.log(day);
@@ -18,7 +18,7 @@ app.get("/",function (req,res) {
                  break;
         case 2 : currday = "Tuesday";
                 break;
-        case 3 : currday = "Wedday";
+        case 3 : currday = "Wednesday";
                 break;
         case 4 : currday = "Thursday";
                 break;
